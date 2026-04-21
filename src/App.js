678ac6583340163975e03f3358c7548211ef1e31
Tho Chau <chauphuoctho2006@gmail.com>
2026-04-21 15:35:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo></Logo>
+      <Form></Form>
+      <PackingList></PackingList>
+      <Stats></Stats>
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>Far Travel</h1>;
+}
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Cần gì cho chuyến đi nhờ</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(e.target.value);
+        }}
+      >
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Item"
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      ></input>
+      <button>Summit</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item(props) {
+  return (
+    <li>
+      {" "}
+      <span style={props.item.packed ? { textDecoration: "line-through" } : {}}>
+        {props.item.quantity} {props.item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>TEST</em>
+    </footer>
+  );
+}
